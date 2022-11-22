f7a0d8231bc916c36b92a40c63d1987ec390565c
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-11-22 23:26:49 +0000

    Parse headers and use in get request

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,46 @@ use clap::Parser;
 use reqwest;
 use serde_json::Value;
 
-// #[derive(Parser,Default,Debug, Clone)]
-// struct Header {
-//     name: String,
-//     value: String,
-// }
+#[derive(Parser,Default,Debug, Clone)]
+struct Header {
+    name: String,
+    value: String,
+}
+
+fn header_parser(s: &str) -> Result<Header, String> {
+    let parts: Vec<&str> = s.split(":").collect();
+    if parts.len() != 2 {
+        return Err(String::from("Invalid header format, must be name:value"));
+    }
+
+    Ok(Header{
+        name: String::from(parts[0]),
+        value: String::from(parts[1]),
+    })
+}
 
 #[derive(Parser,Default,Debug)]
 struct Options {
     url: String,
+    #[clap(short('H'), long("header"), value_parser = header_parser)]
+    header: Option<Vec<Header>>,
 }
 
 #[tokio::main]
 async fn main() {
     let options = Options::parse();
 
-    let response = reqwest::get(options.url)
+    let client = reqwest::Client::new();
+    let mut request_builder = client
+        .get(options.url);
+    if let Some(headers) = options.header {
+        for h in headers {
+            request_builder = request_builder.header(h.name, h.value);
+        }
+    }
+
+    let response = request_builder
+        .send()
         .await
         .unwrap()
         .text()

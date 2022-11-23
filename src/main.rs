d7336dc3b1e02fb2d703775888199a617a26861e
Kashif Soofi <kashif.soofi+git@gmail.com>
2022-11-23 00:46:25 +0000

    Added option for request method

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::str::FromStr;
+
 use clap::Parser;
-use reqwest;
+use reqwest::{self, RequestBuilder, Method};
 use serde_json::Value;
 
 #[derive(Parser,Default,Debug, Clone)]
@@ -24,17 +26,27 @@ fn header_parser(s: &str) -> Result<Header, String> {
 struct Options {
     url: String,
     #[clap(short('H'), long("header"), value_parser = header_parser)]
-    header: Option<Vec<Header>>,
+    headers: Option<Vec<Header>>,
+    #[clap(short('X'), long("request"))]
+    request: Option<String>,
+}
+
+fn create_request_builder(request: Option<String>, url: String) -> RequestBuilder {
+    let client = reqwest::Client::new();
+    if let Some(request) = request {
+        let method = Method::from_str(request.to_uppercase().as_str()).expect("Unsupported request method");
+        client.request(method, url)
+    } else {
+        client.get(url)
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let options = Options::parse();
 
-    let client = reqwest::Client::new();
-    let mut request_builder = client
-        .get(options.url);
-    if let Some(headers) = options.header {
+    let mut request_builder = create_request_builder(options.request, options.url);
+    if let Some(headers) = options.headers {
         for h in headers {
             request_builder = request_builder.header(h.name, h.value);
         }
@@ -43,12 +55,17 @@ async fn main() {
     let response = request_builder
         .send()
         .await
-        .unwrap()
-        .text()
-        .await
         .unwrap();
 
-    let object: Value = serde_json::from_str(&response).unwrap();
-    let pretty_json = serde_json::to_string_pretty(&object).unwrap();
-    println!("{}", pretty_json);
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            let response = response.text().await.unwrap();
+            let object: Value = serde_json::from_str(&response).unwrap();
+            let pretty_json = serde_json::to_string_pretty(&object).unwrap();
+            println!("{}", pretty_json);
+        }
+        _ => {
+            println!("{}", response.text().await.unwrap());
+        }
+    };
 }
